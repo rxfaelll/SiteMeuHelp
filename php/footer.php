c426b4abfa615c24b1ea4f60936ad3e06d4ab281
Rafael Soares <52980773+rxfaelll@users.noreply.github.com>
2019-09-11 13:18:42 -0300

    Add files via upload
    Criando repositório

---
+++ php/footer.php
@@ -0,0 +1,30 @@
+<?php
+    echo '<footer class="darkblue-bg">
+            <div>
+                <span>
+                    <img src="img/logo/logo.png" alt="">
+                </span>
+            </div>
+            <div class="container-links-footer">
+                <a href="">Termos e condições</a>
+                <a href="">Políticas de privacidade</a>
+                <a href="">Seja representante comercial</a>
+                <a href="">Encontre os parceiros mais próximos</a>
+            </div>
+            <div class="container-local-footer">
+                <ul>
+                    <li>Matriz: Av. Doutor Yojiro<br>  Takaoká • 4384<br>  Cj. 5721 • Alphaville<br>  Santana de Parnaíba</li>
+                    <li>Filial: R. Josef Zarzour • 93<br>  Cj. 801 • Centro • Guarulhos</li>
+                </ul>
+            </div>
+            <div>
+                <div class="container-social-footer">
+                    <a href=""><img src="img/icon/facebook2.png" alt="">meuhelp</a>
+                    <a href=""><img src="img/icon/instagram2.png" alt="">@meuhelp_ja</a>
+                    <span>Em breve:</span>
+                    <a href=""><img src="img/icon/googleplay.png" alt=""></a>
+                    <a href=""><img src="img/icon/appstore.png" alt=""></a>
+                </div>
+            </div>        
+        </footer>';
+?>
---
+++ php/header.php
@@ -0,0 +1,19 @@
+<?php
+
+    echo '<header>
+                <span class="logo"></span>
+                <nav class="header-nav">
+                    <a href="">Home</a>
+                    <a href="">Planos</a>
+                    <a href="">Planos mais</a>
+                    <a href="">Benefícios</a>
+                    <a href="">Ajuda</a>
+                </nav>
+                <form action="" class="header-search-form">
+                    <input type="text" name="search" placeholder="buscar" class="search-input">
+                    <button class="search-btn" id="searchBtn"></button>
+                </form>
+            </header>';
+            //Header incompleto.
+
+?>
